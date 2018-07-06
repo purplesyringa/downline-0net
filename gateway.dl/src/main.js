f9720a404dbbae2960b454afa6453f4746d25a2d
Ivanq <imachug@yandex.ru>
2018-07-06 15:03:55 +0300

    [gateway.dl] Register UI

--- gateway.dl/src/main.js
+++ gateway.dl/src/main.js
@@ -1,8 +1,31 @@
 import DLTPServer from "./dltp/server";
 import {zeroFS} from "./dltp/zero";
 
-let cache = null;
 
-const dltp = new DLTPServer(req => {
-	return "Hello from gateway.dl!";
+
+function ui(path) {
+	if(path === "") {
+		// Root
+		return {
+			type: "inputs",
+			header: "Register",
+			items: [
+				["Login", "ivanq"],
+				["Password", "password123"],
+				["Repeat password", "password123"]
+			],
+			submit: "Register"
+		};
+	}
+}
+
+const dltp = new DLTPServer((req, con) => {
+	if(req === "ui") {
+		return ui(con.uiPath || "");
+	} else if(req.startsWith("cd:")) {
+		con.uiPath = cd(rcon.uiPath || "", eq.replace("cd:"));
+		return ui(con.uiPath || "");
+	}
+
+	throw new Error(`Unknown gateway command ${req}`);
 });
